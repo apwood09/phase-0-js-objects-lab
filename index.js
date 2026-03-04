a2fce7180b5bf66112b5d7ec2eee48d406c98319
abbi portwood <apwood09@gmail.com>
2026-03-04 13:10:48 -0500

    added functions to log attendee name, log ticket price, update ticket type/price, remove event property, add checkedIn property & add objects

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-//Write your code here
+const attendee = {
+  attendeeId: 'T001', 
+  name: 'Alice Smith', 
+  event: 'JavaScript Conference', 
+  ticketType: 'VIP', 
+  ticketPrice: 150.00, 
+}; 
 
+// function to log attendee name 
+function logAttendeeName(attendee) {
+  console.log(attendee.name); 
+}
 
+// function to log ticket price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice)
+}
+
+// function to update ticket type
+function updateTicketType(attendee, newType) { 
+    attendee.ticketType = newType; 
+}
+updateTicketType(attendee, 'VIP');
+console.log(attendee.ticketType); 
+
+// function update tiket price
+function updateTicketPrice(attendee, newPrice) { 
+  attendee.ticketPrice = newPrice; 
+}
+expect(attendee).toHaveProperty("ticketPrice", 150);
+
+// function remove event property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+} 
+console.log(attendee); 
+
+// add checked in proprty
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true; 
+}
+addCheckedInProperty(attendee);
+console.log(attendee); 
 
 //Needed for the tests to work. Don't modify
 module.exports = {
